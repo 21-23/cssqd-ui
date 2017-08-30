fd203d17727c8b1fe4d350136e86c798f31eba25
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-08-31 01:00:28 +0300

    add base webpack and storybook configuration

---
+++ .storybook/config.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+import { configure, addDecorator } from '@storybook/react';
+
+import { Layout } from '../src/components/Layout';
+
+addDecorator(story => (
+  <Layout>
+    { story() }
+  </Layout>
+));
+
+function loadStories() {
+
+}
+
+configure(loadStories, module);
