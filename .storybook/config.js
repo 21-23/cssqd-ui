08c15a8ca57dbec5b9fde7ce49640cdd647f5e2c
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-09-18 15:29:05 +0300

    indent_size 2 -> 4

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,13 +4,13 @@ import { configure, addDecorator } from '@storybook/react';
 import { Layout } from '../src/components/Layout';
 
 addDecorator(story => (
-  <Layout>
-    { story() }
-  </Layout>
+    <Layout>
+        { story() }
+    </Layout>
 ));
 
 function loadStories() {
-
+    require('../src/components/Header/Header.story')
 }
 
 configure(loadStories, module);
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,16 +1,16 @@
 const config = {
-  resolve: {
-    alias: {
-      react: 'preact-compat',
-      'react-dom': 'preact-compat',
+    resolve: {
+        alias: {
+            react: 'preact-compat',
+            'react-dom': 'preact-compat',
+        },
     },
-  },
-  module: {
-    rules: [{
-      test: /(.png$)/,
-      loader: 'url-loader',
-    }]
-  }
+    module: {
+        rules: [{
+            test: /(.png$)/,
+            loader: 'url-loader',
+        }]
+    }
 };
 
 module.exports = config;
