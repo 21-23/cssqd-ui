41648bad27a2bb9521c9145eef4801b06ee38ce4
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-09-22 22:08:15 +0300

    implement header component (#15)
    * implement header component
    
    * make header more configurable
    
    * remove yarn.lock from .gitignore
    
    * pass only backgroundImageUrl instead of the whole style object

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import { h } from 'preact';
+import Icon from 'react-fontawesome';
+
+const Header = ({ productName, username, backgroundImageUrl }) => (
+    <header style={{ backgroundImage: `url(${backgroundImageUrl})` }}>
+        <span>{ productName }</span>
+
+        <span>
+            <Icon name="user" className="user-icon" />
+            { username }
+        </span>
+        <style jsx>{`
+            header {
+                padding: 10px;
+                flex: 1;
+                display: flex;
+                justify-content: space-between;
+                background-size: auto 100%;
+                color: white;
+                box-sizing: border-box;
+            }
+
+            .user-icon {
+                margin-right: 5px;
+            }
+        `}</style>
+    </header>
+);
+
+export { Header };
