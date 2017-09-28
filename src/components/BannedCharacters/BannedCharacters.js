164554af79eb4454e2b6034244d1fe79a6bee6dc
Dmitry Kantin <kantindm@gmail.com>
2017-09-29 00:55:31 +0300

    add scale for highlighted chars
    move display constants and colors in a separate file
    add a check for chars presence

--- src/components/BannedCharacters/BannedCharacters.js
+++ src/components/BannedCharacters/BannedCharacters.js
@@ -1,52 +1,57 @@
 import { h } from 'preact';
 import classNames from 'classnames';
+import { DisplayConstants, Colors } from './banned-characters-style-constants';
 
-const FONT_SIZE = 18;
+const BannedCharacters = ({ bannedCharacters = [], highlightedCharacters = [] }) => {
+    if (bannedCharacters.length) {
+        return (<div class="banned-characters">
+            <span class="label">Banned chars </span>
+            { bannedCharacters.map(char => BannedCharactrer(char, highlightedCharacters.includes(char))) }
 
-const BannedCharacters = ({ bannedCharacters = [], highlightedCharacters = [] }) => (
-    <div class="banned-characters">
-        <span class="label">Banned chars </span>
-        { bannedCharacters.map(char => BannedCharactrer(char, highlightedCharacters.includes(char))) }
+            <style jsx>{`
+                .banned-characters {
+                    display: inline-flex;
+                    align-items: center;
+                }
 
-        <style jsx>{`
-            .banned-characters {
-                display: inline-flex;
-                align-items: center;
-            }
+                .label {
+                    font-weight: 600;
+                    font-family: sans-serif;
+                    font-size: ${DisplayConstants.FONT_SIZE}px;
+                    margin-right: 3px;
+                    color: ${Colors.LABEL};
+                    display: inline-block
+                }
 
-            .label {
-                font-weight: 600;
-                font-family: sans-serif;
-                font-size: ${FONT_SIZE}px;
-                margin-right: 3px;
-                color: #b3decc;
-                display: inline-block
-            }
+                .banned-character {
+                    display: inline-block;
+                    font-family: monospace;
+                    font-weight: 600;
+                    font-size: ${DisplayConstants.FONT_SIZE}px;
+                    color: ${Colors.CHARACTER};
+                    margin: 0 3px;
+                    background-color: ${Colors.CHARACTER_BACKGROUND};
+                    width: 23px;
+                    height: 23px;
+                    border-radius: 4px;
+                    text-align: center;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                }
 
-            .banned-character {
-                display: inline-block;
-                font-family: monospace;
-                font-weight: 600;
-                font-size: ${FONT_SIZE}px;
-                color: #FFFFFF;
-                margin: 3px;
-                background-color: #3A706A;
-                width: 23px;
-                height: 23px;
-                border-radius: 4px;
-                text-align: center;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
+                .highlighted {
+                    background-color: ${Colors.HIGHLIGHTED_CHARACTER_BACKGROUND};
+                    border: 2px solid ${Colors.HIGHLIGHTED_CHARACTER_BORDER};
+                    transform: scale(${DisplayConstants.HIGHLIGHTED_CHAR_SCALE_COEFFICIENT});
+                    margin: 0 7px;
+                }
+            `}</style>
+        </div>)
+    }
 
-            .highlighted {
-                background-color: #775957;
-                border: 2px solid #B25254;
-            }
-        `}</style>
-    </div>
-);
+    return null;
+};
 
 const BannedCharactrer = (char, isHighlighted) => {
     const bannedCharacterClasses = classNames('banned-character', {
---
+++ src/components/BannedCharacters/banned-characters-style-constants.js
@@ -0,0 +1,12 @@
+export const Colors = {
+    LABEL: '#b3decc',
+    CHARACTER: '#ffffff',
+    CHARACTER_BACKGROUND: '#3a706a',
+    HIGHLIGHTED_CHARACTER_BACKGROUND: '#775957',
+    HIGHLIGHTED_CHARACTER_BORDER: '#b25254',
+};
+
+export const DisplayConstants = {
+    FONT_SIZE: 16,
+    HIGHLIGHTED_CHAR_SCALE_COEFFICIENT: 1.35,
+};
