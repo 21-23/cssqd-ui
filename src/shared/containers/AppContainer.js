a6e39b1de8eaaefa61ef2b26a72e466ac80010eb
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-10-01 23:19:51 +0300

    fix shared containers

--- src/shared/containers/AppContainer.js
+++ src/shared/containers/AppContainer.js
@@ -1,5 +1,7 @@
 import { h } from 'preact';
 import { Header } from './HeaderContainer';
+import { Layout } from '../../components/Layout';
+
 
 const AppContainer = ({ children }) => (
     <Layout>
--- src/shared/containers/HeaderContainer.js
+++ src/shared/containers/HeaderContainer.js
@@ -1,14 +1,13 @@
 import { h } from 'preact';
 import { connect } from 'preact-redux';
-import { Header as PureHeader } from '../components/Header/Header';
+import { Header as PureHeader } from '../../components/Header/Header';
 
-const backgroundImageUrl = require('../assets/images/header-bg.png');
+const backgroundImageUrl = require('../../assets/images/header-bg.png');
 
 const Header = connect(state => ({
     username: state.username,
-}))
-(({ username }) => (
-    <Header
+}))(({ username }) => (
+    <PureHeader
         productName="CSS Quickdraw"
         username={username}
         backgroundImageUrl={backgroundImageUrl}
