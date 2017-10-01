73249f67bfcb65d43bd9b6b26b3515e48b7999ae
Andrei Yanovich <yanovich.andrei@gmail.com>
2017-10-01 18:35:03 +0200

    added storybook config plugin (#23)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,13 @@
 import { h } from 'preact';
 import { configure, addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { setOptions } from '@storybook/addon-options';
 
 import { Layout } from '../src/components/Layout';
+
+setOptions({
+    downPanelInRight: true
+});
 
 addDecorator(story => (
     <Layout>
