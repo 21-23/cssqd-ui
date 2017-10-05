13743ddea341ca92d4e85ea32b1315c8641c9bd6
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-10-05 20:55:43 +0300

    export reducer

--- cli/app-module-template/reducers/__#{name}-reducer.js
+++ cli/app-module-template/reducers/__#{name}-reducer.js
@@ -2,7 +2,7 @@ import * as Actions from '../actions/#{name}-actions';
 
 const initialState = null;
 
-function #{camelCaseName}Reducer(state = initialState, action) {
+export function #{camelCaseName}Reducer(state = initialState, action) {
     switch (action.type) {
         default: return state;
     }
