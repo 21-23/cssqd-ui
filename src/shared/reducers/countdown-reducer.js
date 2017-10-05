5b54c9de2f9f4a5eba7a092f43cd0e6ad9cfd50c
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-10-06 00:11:52 +0300

    add countdown actions & reducer (#25)

---
+++ src/shared/reducers/countdown-reducer.js
@@ -0,0 +1,12 @@
+import * as Actions from '../actions/countdown-actions';
+
+const initialState = null;
+
+export function countdownReducer(state = initialState, action) {
+    switch (action.type) {
+        case Actions.SET_TIME_REMAINING:
+            return action.payload;
+
+        default: return state;
+    }
+}
