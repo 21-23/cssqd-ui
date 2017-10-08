1194b8b7aa75b96fcb7d91020ce0554f0a52923e
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-10-08 18:09:06 +0300

    export store to window in non-production env

--- cli/app-module-template/#{name}-store.js
+++ cli/app-module-template/#{name}-store.js
@@ -2,4 +2,9 @@ import { createStore } from 'redux';
 import { #{camelCaseName}Reducer } from './reducers/#{name}-reducer.js';
 
 const store = createStore(#{camelCaseName}Reducer);
+
+if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+}
+
 export { store };
--- src/game/game-store.js
+++ src/game/game-store.js
@@ -2,6 +2,9 @@ import { createStore } from 'redux';
 import { gameReducer } from './reducers/game-reducer.js';
 
 const store = createStore(gameReducer);
-window.store = store;
+
+if (process.env.NODE_ENV !== 'production') {
+    window.store = store;
+}
 
 export { store };
