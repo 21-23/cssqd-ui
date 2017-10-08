d71df4fb2abed08c626dc960d6e9ffbcf87e885b
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-10-08 19:50:11 +0300

    add selector input and connect it with redux

--- src/game/game-store.js
+++ src/game/game-store.js
@@ -1,7 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
 import { gameReducer } from './reducers/game-reducer.js';
 
-const store = createStore(gameReducer);
+const store = createStore(
+    gameReducer,
+    applyMiddleware(logger),
+);
 
 if (process.env.NODE_ENV !== 'production') {
     window.store = store;
