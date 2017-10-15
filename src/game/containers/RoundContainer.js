2e76485e78a01b49c129298a701cf666e70c3f01
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-10-15 18:25:20 +0300

    add MarkupRenderer to game page

--- src/game/containers/RoundContainer.js
+++ src/game/containers/RoundContainer.js
@@ -4,6 +4,9 @@ import { connect } from 'preact-redux';
 import { RoundLayout } from '../components/RoundLayout';
 import { Countdown } from '../../components/Countdown/Countdown';
 import { SelectorInput } from '../../components/SelectorInput/SelectorInput';
+import { MarkupRenderer } from '../../components/MarkupRenderer/MarkupRenderer';
+import { withDotSelectionIndicator } from '../../components/MarkupRenderer/with-dot-selection-indicator';
+import { Line } from '../../components/MarkupRenderer/Line';
 
 import * as RoundPhase from '../../shared/constants/round-phase';
 import { DURATION } from '../../shared/constants/round';
@@ -11,9 +14,15 @@ import { DURATION } from '../../shared/constants/round';
 import { setSelector } from '../actions/solution-actions';
 
 
-const PureRound = ({ roundPhase, timeRemaining, selector, setSelector }) =>
-    roundPhase !== RoundPhase.IN_PROGRESS && roundPhase !== RoundPhase.FINISHED ? null :
-
+const PureRound = ({
+    roundPhase,
+    timeRemaining,
+    selector,
+    setSelector,
+    markup,
+    expectedSelection,
+    actualSelection,
+}) => roundPhase !== RoundPhase.IN_PROGRESS && roundPhase !== RoundPhase.FINISHED ? null :
     <RoundLayout>
         <SelectorInput
             value={selector}
@@ -24,12 +33,20 @@ const PureRound = ({ roundPhase, timeRemaining, selector, setSelector }) =>
             timeAmount={DURATION}
             timeRemaining={timeRemaining}
         />
+        <MarkupRenderer
+            source={markup}
+            expectedSelection={expectedSelection}
+            actualSelection={actualSelection}
+        />
     </RoundLayout>
 
 const RoundContainer = connect(state => ({
     roundPhase: state.roundPhase,
     timeRemaining: state.countdown,
     selector: state.solution.selector,
+    markup: state.puzzle.markup,
+    expectedSelection: state.puzzle.expectedSelection,
+    actualSelection: state.solution.selection,
 }), {
     setSelector,
 })(PureRound);
