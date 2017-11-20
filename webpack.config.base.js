2b4b106b7eb9a8685fa4de6de4909a6a76887947
Kiril Knysh <k.knysh@gmail.com>
2017-11-20 23:33:14 +0100

    Add logo;

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 const config = {
     resolve: {
         modules: [path.resolve(__dirname, "src"), "node_modules"],
@@ -17,7 +19,28 @@ const config = {
             test: /(\.(png|jpg)$)/,
             loader: 'url-loader',
         }]
-    }
+    },
+    plugins: [
+        new FaviconsWebpackPlugin({
+            logo: 'assets/images/logo.png',
+            persistentCache: true,
+            inject: true,
+            background: '#fff',
+            title: 'CSS Quick Draw',
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
+            }
+        }),
+    ],
 };
 
 module.exports = config;
