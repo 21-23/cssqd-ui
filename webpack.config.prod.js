d9f1f81cabcad52ce80928c02f5aa27c26bef3f3
Kiril Knysh <k.knysh@gmail.com>
2017-11-23 00:10:57 +0100

    Do not use source maps in prod build;

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,6 +10,7 @@ const ownConfig = {
         path: path.join(__dirname, 'dist-prod'),
         filename: '[name].bundle.js',
     },
+    devtool: 'nosources-source-map',
 };
 
 module.exports = merge(baseConfig, ownConfig);
