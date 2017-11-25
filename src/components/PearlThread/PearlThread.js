3a5286c52e9e32a4e4ab06643e5d4e7aa90b304d
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-11-25 17:48:29 +0300

    fix pearl thread (#46)
    * fix pearl thread
    
    * Update PearlThread.story.js

--- src/components/PearlThread/PearlThread.js
+++ src/components/PearlThread/PearlThread.js
@@ -3,41 +3,114 @@ import classNames from 'classnames';
 import Icon from 'react-fontawesome';
 import { Colors } from './pearl-thread-style-constants';
 
-function onPearlThreadClick(onPearlClick, event) {
-    let target = event.target;
+const FuturePearl = ({ index }) => (
+    <span>
+        { index }
+        <style jsx>{`
+            span {
+                color: rgba(255, 255, 255, 0.5);
+                padding: 5px;
+                cursor: pointer;
+                margin-left: -5px;
+            }
+        `}</style>
+    </span>
+);
 
-    while (target) {
-        const pearlIndex = target.dataset && target.dataset.pearlIndex;
+const PastPearl = () => (
+    <span className="pearl">
+        <Icon name="check" />
 
-        if (pearlIndex) {
-            onPearlClick(+pearlIndex);
-        }
+        <style jsx>{`
+            .pearl {
+                padding: 2px;
+                border-radius: 50%;
+                width: 25px;
+                height: 25px;
+                box-sizing: border-box;
+                border: 2px solid rgba(255, 255, 255, 0.3);
+                border-radius: 50%;
+                cursor: pointer;
+            }
+        `}</style>
+    </span>
+);
 
-        target = target.parentNode;
+const TitlePearl = ({ isFirst, isLast, title }) => {
+    const style = {};
+
+    if (!isFirst && !isLast) {
+        style.transform = 'translateX(-50%)';
     }
+
+    if (isLast) {
+        style.transform = 'translateX(-100%)';
+    }
+
+    return (
+        <span className="title" style={style}>
+            { title }
+
+            <style jsx>{`
+                .title {
+                    position: absolute;
+                    text-align: center;
+                    cursor: pointer;
+                }
+            `}</style>
+        </span>
+    );
 }
 
-const PearlThread = ({ items = [], activeIndex = 0, onPearlClick }) => {
-    const progressPercentage = (activeIndex / (items.length - 1)) * 100;
+const Pearl = ({ isPast, index, title, isFirst, isLast, onClick }) => {
+    let content = <FuturePearl index={index + 1} />;
+
+    if (isPast) {
+        content = <PastPearl />
+    }
+
+    if (title) {
+        content = <TitlePearl isFirst={isFirst} isLast={isLast} title={title} />
+    }
+
+    return (
+        <div className="pearl-container">
+            <span onClick={() => onClick(index)}>
+                { content }
+            </span>
+
+            <style jsx>{`
+                .pearl-container {
+                    position: relative;
+                    margin-top: 10px;
+                    width: 0;
+                    height: 0;
+                    color: #f8d940;
+                }
+            `}</style>
+        </div>
+    );
+}
+
+const PearlThread = ({ itemsCount, activeIndex = 0, activeTitle, onPearlClick }) => {
+    const progressPercentage = (activeIndex / (itemsCount - 1)) * 100;
     const safeProgressPercentage = Math.min(progressPercentage, 100);
-    const isClickable = typeof onPearlClick === 'function';
-    const onContainerClick = isClickable ? onPearlThreadClick.bind(null, onPearlClick) : null;
 
     return (
         <div className="pearl-thread">
             <div className="progress-bar" style={{ width: `${safeProgressPercentage}%` }} ></div>
-
-            <div className="puzzle-progress-container"
-                onClick={onContainerClick}>
-                {items.map((item, index) => Pearl({
-                    caption: item.caption,
-                    index,
-                    activeIndex,
-                    isClickable,
-                    onPearlClick,
-                }))}
+            <div className="pearls-container">
+                { new Array(itemsCount).fill(0).map((_, index) => (
+                    <Pearl
+                        isPast={index < activeIndex}
+                        title={activeIndex === index ? activeTitle : null}
+                        index={index}
+                        isFirst={index === 0}
+                        isLast={index === itemsCount - 1}
+                        onClick={onPearlClick}
+                    />
+                )) }
             </div>
-
 
             <style jsx>{`
                 .pearl-thread {
@@ -57,66 +130,14 @@ const PearlThread = ({ items = [], activeIndex = 0, onPearlClick }) => {
                     height: 3px;
                     transition: width 1s;
                 }
+
+                .pearls-container {
+                    display: flex;
+                    justify-content: space-between;
+                }
             `}</style>
         </div>
     );
 }
-
-const Pearl = ({ caption, index, activeIndex, isClickable }) => {
-    let content;
-
-    if (index < activeIndex) {
-        content = <Icon name="check"/>;
-    } else if (index === activeIndex) {
-        content = caption;
-    } else {
-        content = index + 1;
-    }
-
-    const itemClass = classNames('pearl', {
-        'solved': index < activeIndex,
-        'current': index === activeIndex,
-        'future': index > activeIndex,
-        'clickable': isClickable,
-    });
-
-    return (
-        <div className={itemClass} data-pearl-index={index}>
-
-            { content }
-
-            <style jsx>{`
-                .pearl {
-                    box-sizing: border-box;
-                    width: 25px;
-                    height: 25px;
-                    line-height: 25px;
-                    text-align: center;
-                    color: ${Colors.UNSOLVED_PEARL_COLOR};
-                }
-
-                .pearl.solved {
-                    border: 2px solid #4c8580;
-                    border-radius: 50%;
-                }
-
-                .pearl.solved,
-                .pearl.current {
-                    color: ${Colors.SOLVED_PEARL_COLOR};
-                }
-
-                .pearl.current {
-                    width: auto;
-                    min-width: 25px;
-                    white-space: nowrap;
-                }
-
-                .clickable {
-                    cursor: pointer;
-                }
-            `}</style>
-        </div>
-    );
-};
 
 export { PearlThread };
--- src/components/PearlThread/PearlThread.story.js
+++ src/components/PearlThread/PearlThread.story.js
@@ -1,41 +1,22 @@
 import { h } from 'preact';
 import { storiesOf } from '@storybook/react';
-import { number, boolean } from '@storybook/addon-knobs';
+import { number, text } from '@storybook/addon-knobs';
 
 import { PearlThread } from './PearlThread';
-
-const rounds = [
-    'Lorem',
-    'ipsum',
-    'dolor sit amet',
-    'consectetur',
-    'adipisicing elit, Debitis culpa aspernatur repellendus, vel quis adipisci voluptates',
-    'cum',
-    'illo',
-    'quidem',
-    'dolor',
-    'a',
-     'quibusdam',
-     'quibusdam',
-].map(caption => ({ caption }));
 
 storiesOf('PearlThread', module)
     .add('default', () => (
         <PearlThread
-            items={rounds}
+            itemsCount={10}
             activeIndex={number('activeIndex', 5)}
+            activeTitle={text('activeTitle', 'Lorem')}
         />
     ))
-    .add('clickable', () => (
+    .add('with long title', () => (
         <PearlThread
-            items={rounds}
+            itemsCount={10}
             activeIndex={number('activeIndex', 5)}
+            activeTitle={text('activeTitle', 'Lorem ipsum dolor')}
             onPearlClick={console.log}
-        />
-    ))
-    .add('overpopulated', () => (
-        <PearlThread
-            items={rounds.concat(rounds)}
-            activeIndex={number('activeIndex', 5)}
         />
     ));
