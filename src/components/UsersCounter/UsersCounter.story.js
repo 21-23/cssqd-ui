bd250dcf8ed88b194e6c0dc91d6337a083690181
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-11-26 01:45:21 +0300

    implement UsersCounter

---
+++ src/components/UsersCounter/UsersCounter.story.js
@@ -0,0 +1,24 @@
+import { h } from 'preact';
+import { storiesOf } from '@storybook/react';
+import { number, text } from '@storybook/addon-knobs';
+
+
+import { UsersCounter } from './UsersCounter';
+
+storiesOf('UsersCounter', module)
+    .addDecorator(story => (
+        <div>
+            { story() }
+            <style jsx>{`
+                div {
+                    color: white;
+                }
+            `}</style>
+        </div>
+    ))
+    .add('default', () => (
+        <UsersCounter
+            count={number('count', 10)}
+            label={text('label', 'joined')}
+        />
+    ));
