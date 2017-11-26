71d7f08248918e39bf73eb398c7a1ab1aa39f42b
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-11-26 15:52:03 +0300

    implement Button component (#48)
    * implement Button component
    
    * replace icon prop with iconLeft/iconRight

---
+++ src/components/Button/Button.story.js
@@ -0,0 +1,29 @@
+import { h } from 'preact';
+import { storiesOf } from '@storybook/react';
+
+import { Button } from './Button';
+
+storiesOf('Button', module)
+    .addDecorator(story => (
+        <div>
+            { story() }
+
+            <style jsx>{`
+                div {
+                    font-size: 20px;
+                }
+            `}</style>
+        </div>
+    ))
+    .add('Stop level', () => (
+        <Button
+            iconLeft="stop"
+            text="Stop level"
+        />
+    ))
+    .add('Next level', () => (
+        <Button
+            iconRight="arrow-right"
+            text="Next level"
+        />
+    ));
