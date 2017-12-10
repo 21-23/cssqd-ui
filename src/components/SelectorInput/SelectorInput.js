7ad0064d95cccda18c2e084de31db2cb9aa2717e
Andrei Volchenko <volchenko.andrei@gmail.com>
2017-12-10 16:34:51 +0300

    focus selector input after round start

--- src/components/SelectorInput/SelectorInput.js
+++ src/components/SelectorInput/SelectorInput.js
@@ -1,54 +1,71 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
 import * as Colors from './selector-input-color-palette';
 
-const SelectorInput = ({
-    onInput,
-    disabled,
-    value,
-    maxLength = 128,
-    placeholder = 'Enter your selector here...'
-}) => (
-    <input
-        type="text"
-        onInput={e => onInput(e.target.value)}
-        disabled={disabled}
-        placeholder={placeholder}
-        value={value}
-        maxLength={maxLength}
-    >
-        <style jsx>{`
-            input {
-                width: 100%;
-                box-sizing: border-box;
-                outline: none;
-                padding: 20px;
-                background: ${Colors.BACKGROUND};
-                border: 1px solid ${Colors.BORDER};
-                border-radius: 3px;
-                color: ${Colors.TEXT};
-                align-self: center;
-                font-family: monospace;
-                font-size: 16px;
-            }
+class SelectorInput extends Component {
+    render() {
+        const {
+            onInput,
+            disabled,
+            value,
+            maxLength = 128,
+            placeholder = 'Enter your selector here...'
+        } = this.props;
 
-            input:focus {
-                border: 1px solid ${Colors.BORDER_FOCUSED};
-                box-shadow:
-                    0 0 4px 0px ${Colors.BOX_SHADOW_FOCUSED} inset,
-                    0 0 4px 0px ${Colors.BOX_SHADOW_FOCUSED};
-            }
+        return (
+            <input
+                type="text"
+                onInput={e => onInput(e.target.value)}
+                disabled={disabled}
+                placeholder={placeholder}
+                value={value}
+                maxLength={maxLength}
+                ref={this.refInput}
+            >
+                <style jsx>{`
+                    input {
+                        width: 100%;
+                        box-sizing: border-box;
+                        outline: none;
+                        padding: 20px;
+                        background: ${Colors.BACKGROUND};
+                        border: 1px solid ${Colors.BORDER};
+                        border-radius: 3px;
+                        color: ${Colors.TEXT};
+                        align-self: center;
+                        font-family: monospace;
+                        font-size: 16px;
+                    }
 
-            input:disabled {
-                background: ${Colors.BACKGROUND_DISABLED};
-                color: ${Colors.TEXT_DISABLED};
-            }
+                    input:focus {
+                        border: 1px solid ${Colors.BORDER_FOCUSED};
+                        box-shadow:
+                            0 0 4px 0px ${Colors.BOX_SHADOW_FOCUSED} inset,
+                            0 0 4px 0px ${Colors.BOX_SHADOW_FOCUSED};
+                    }
 
-            input::placeholder {
-                color: ${Colors.PLACEHOLDER};
-            }
-        `}</style>
-    </input>
-);
+                    input:disabled {
+                        background: ${Colors.BACKGROUND_DISABLED};
+                        color: ${Colors.TEXT_DISABLED};
+                    }
+
+                    input::placeholder {
+                        color: ${Colors.PLACEHOLDER};
+                    }
+                `}</style>
+            </input>
+        );
+    }
+
+    refInput = (node) => {
+        this.input = node;
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.disabled && !this.props.disabled) {
+            this.input.focus();
+        }
+    }
+}
 
 export { SelectorInput };
