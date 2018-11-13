63fb22745a836a34af2b28621b76a983ba6ee03b
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2018-11-14 00:21:00 +0300

    fix: display solution time while round is in progress (#86)

--- src/game-master/selectors/score-selectors.js
+++ src/game-master/selectors/score-selectors.js
@@ -3,23 +3,32 @@ import { createSelector } from 'reselect';
 import { participants } from './participants-selectors';
 import { roundStarted } from '../../shared/selectors/round-phase-selectors';
 
-const root = state => state.score;
-export const visibleScore = createSelector([root], score => score.visibleScore);
+const CORRECT_SOLUTION = 'correct';
 
-export const aggregateScore = createSelector([participants], participants =>
-    participants.map(participant => ({
-        displayName: participant.displayName,
-        participantId: participant.participantId,
-        aggregateScore: participant.aggregateScore,
-    }))
+const root = state => state.score;
+export const visibleScore = createSelector(
+    [root],
+    score => score.visibleScore
 );
 
-export const roundScore = createSelector([participants, roundStarted], (participants, roundStarted) =>
-    participants.map(participant => ({
-        displayName: participant.displayName,
-        participantId: participant.participantId,
-        code: roundStarted ? participant.inputLength : (participant.solution && participant.solution.code) || '',
-        correct: participant.solution && participant.solution.correct,
-        time: roundStarted ? null : (participant.solution && participant.solution.time) || 0,
-    }))
+export const aggregateScore = createSelector(
+    [participants],
+    participants =>
+        participants.map(participant => ({
+            displayName: participant.displayName,
+            participantId: participant.participantId,
+            aggregateScore: participant.aggregateScore,
+        }))
+);
+
+export const roundScore = createSelector(
+    [participants, roundStarted],
+    (participants, roundStarted) =>
+        participants.map(({ displayName, participantId, inputLength, solution }) => ({
+            displayName: displayName,
+            participantId: participantId,
+            code: roundStarted ? inputLength : (solution && solution.code) || '',
+            correct: solution && solution.correct,
+            time: roundStarted ? (solution && solution.correct === CORRECT_SOLUTION && solution.time) || 0 : null,
+        }))
 );
